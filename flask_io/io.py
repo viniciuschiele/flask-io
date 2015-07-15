1d79929dde0176abfeae61f034522d2b39c55d97
Vinicius Chiele <vinicius.chiele@gmail.com>
2015-07-15 22:55:22 +0100

    Refactored

--- flask_io/io.py
+++ flask_io/io.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from flask import request
 from functools import wraps
 from inspect import isfunction
 from .parsers import BooleanParser
@@ -6,11 +7,6 @@ from .parsers import DateTimeParser
 from .parsers import PrimitiveParser
 from .errors import InvalidArgumentError
 from .errors import RequiredArgumentError
-from .sources import BodySource
-from .sources import CookieSource
-from .sources import FormSource
-from .sources import HeaderSource
-from .sources import QuerySource
 
 
 class FlaskIO(object):
@@ -33,73 +29,88 @@ class FlaskIO(object):
     def init_app(self, app):
         pass
 
-    def from_body(self, param_name, type_, default=None, required=False, validate=None):
-        return self.__from_source(BodySource, param_name, type_, default, required, False, validate, param_name)
-
-    def from_cookie(self, param_name, type_, default=None, required=False, multiple=False, validate=None, arg_name=None):
-        return self.__from_source(CookieSource, param_name, type_, default, required, multiple, validate, arg_name)
-
-    def from_form(self, param_name, type_, default=None, required=False, multiple=False, validate=None, arg_name=None):
-        return self.__from_source(FormSource, param_name, type_, default, required, multiple, validate, arg_name)
-
-    def from_header(self, param_name, type_, default=None, required=False, multiple=False, validate=None, arg_name=None):
-        return self.__from_source(HeaderSource, param_name, type_, default, required, multiple, validate, arg_name)
-
-    def from_query(self, param_name, type_, default=None, required=False, multiple=False, validate=None, arg_name=None):
-        return self.__from_source(QuerySource, param_name, type_, default, required, multiple, validate, arg_name)
-
-    def __parse(self, source, name, type_, default, required, multiple, validate):
-        try:
-            values = source.get_value(name, multiple)
-
-            parser = self.__parsers.get(type_)
-
-            if isinstance(values, list):
-                if len(values) == 0:
-                    values.append(None)
-            else:
-                values = [values]
-
-            for i in range(len(values)):
-                value = values[i]
-                value = None if value is None else parser.parse(value)
-
-                if value is None:
-                    if required:
-                        raise RequiredArgumentError(name)
-
-                    if default:
-                        if isfunction(default):
-                            value = default()
-                        else:
-                            value = default
-
-                if validate:
-                    try:
-                        success = validate(value)
-                    except Exception as e:
-                        raise InvalidArgumentError(name, e)
-
-                    if not success:
-                        raise InvalidArgumentError(name)
-
-                values[i] = value
-
-            if multiple:
-                return values
-
-            return values[0]
-        except Exception as e:
-            if isinstance(e, (InvalidArgumentError, RequiredArgumentError)):
-                raise
-            raise InvalidArgumentError(name, e)
-
-    def __from_source(self, source_cls, param_name, type_, default=None, required=False, multiple=False, validate=None, arg_name=None):
+    def from_cookie(self, param_name, param_type, default=None, required=False, multiple=False, validate=None, arg_name=None):
         def decorator(func):
             @wraps(func)
             def wrapper(*args, **kwargs):
-                source = source_cls()
-                kwargs[param_name] = self.__parse(source, arg_name or param_name, type_, default, required, multiple, validate)
+                self.__parse_arg_into_param(kwargs, param_name, param_type, request.cookies, arg_name, default, required, multiple, validate)
                 return func(*args, **kwargs)
             return wrapper
         return decorator
+
+    def from_form(self, param_name, param_type, default=None, required=False, multiple=False, validate=None, arg_name=None):
+        def decorator(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                self.__parse_arg_into_param(kwargs, param_name, param_type, request.form, arg_name, default, required, multiple, validate)
+                return func(*args, **kwargs)
+            return wrapper
+        return decorator
+
+    def from_header(self, param_name, param_type, default=None, required=False, multiple=False, validate=None, arg_name=None):
+        def decorator(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                self.__parse_arg_into_param(kwargs, param_name, param_type, request.headers, arg_name, default, required, multiple, validate)
+                return func(*args, **kwargs)
+            return wrapper
+        return decorator
+
+    def from_query(self, param_name, param_type, default=None, required=False, multiple=False, validate=None, arg_name=None):
+        def decorator(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                self.__parse_arg_into_param(kwargs, param_name, param_type, request.args, arg_name, default, required, multiple, validate)
+                return func(*args, **kwargs)
+            return wrapper
+        return decorator
+
+    def __parse_arg_into_param(self, params, param_name, param_type, args, arg_name, default, required, multiple, validate):
+        if not arg_name:
+            arg_name = param_name
+
+        parser = self.__parsers.get(param_type)
+
+        if not parser:
+            raise Exception('Parameter type \'%s\'does not have a parser.' % str(param_type))
+
+        try:
+            if multiple:
+                arg_values = args.getlist(arg_name) or [None]
+                param_values = []
+                for arg_value in arg_values:
+                    param_values.append(self.__parse_arg(parser, arg_name, arg_value, default, required, validate))
+                params[param_name] = param_values
+            else:
+                arg_value = args.get(arg_name)
+                params[param_name] = self.__parse_arg(parser, arg_name, arg_value, default, required, validate)
+        except Exception as e:
+            if isinstance(e, (InvalidArgumentError, RequiredArgumentError)):
+                raise
+            raise InvalidArgumentError(arg_name, e)
+
+    @staticmethod
+    def __parse_arg(parser, arg_name, arg_value, default, required, validate):
+        if arg_value:
+            arg_value = parser.parse(arg_value)
+
+        if not arg_value:
+            if required:
+                raise RequiredArgumentError(arg_name)
+
+            if default:
+                if isfunction(default):
+                    arg_value = default()
+                else:
+                    arg_value = default
+
+        if validate:
+            try:
+                success = validate(arg_value)
+            except Exception as e:
+                raise InvalidArgumentError(arg_name, e)
+
+            if not success:
+                raise InvalidArgumentError(arg_name)
+
+        return arg_value
