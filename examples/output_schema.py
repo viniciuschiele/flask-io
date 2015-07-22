1c83c6da5e5af30a194442fb5534473d7e080eaa
viniciuschiele <vinicius.chiele@gmail.com>
2015-07-22 16:47:54 +0100

    Added new example

---
+++ examples/output_schema.py
@@ -0,0 +1,60 @@
+# Copyright 2015 Vinicius Chiele. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from flask import Flask
+from flask import jsonify
+from flask_io import FlaskIO
+from flask_io.errors import ValidationError
+from marshmallow import Schema, fields
+
+app = Flask(__name__)
+
+io = FlaskIO()
+io.init_app(app)
+
+
+class User(object):
+    def __init__(self, username):
+        self.username = username
+
+
+class UserSchema(Schema):
+    username = fields.String()
+
+
+@app.route('/users')
+@io.from_query('username', str, required=True)
+@io.from_query('max_results', int, default=10)
+@io.render(schema=UserSchema)
+def list_users(username, max_results):
+    users = []
+
+    for i in range(max_results):
+        users.append(User('user' + str(i)))
+
+    if username:
+        users = [user for user in users if user.username.find(username) > -1]
+
+    return users
+
+
+@app.errorhandler(ValidationError)
+def validation_handler(error):
+    response = jsonify(error_message=error.message)
+    response.status_code = 400
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
