07281eb0a8df798c6aaca7ea3e4f7043d8f2045e
viniciuschiele <vinicius.chiele@gmail.com>
2015-07-23 14:33:22 +0100

    Added example of schema on request body

---
+++ examples/input_schema.py
@@ -0,0 +1,54 @@
+# Copyright 2015 Vinicius Chiele. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from flask import Flask
+from flask import jsonify
+from flask_io import FlaskIO
+from flask_io.errors import ValidationError
+from marshmallow import Schema, fields
+
+app = Flask(__name__)
+
+io = FlaskIO()
+io.init_app(app)
+
+
+class User(object):
+    def __init__(self, username):
+        self.username = username
+
+
+class UserSchema(Schema):
+    username = fields.String()
+
+    def make_object(self, data):
+        return User(**data)
+
+
+@app.route('/users', methods=['POST'])
+@io.from_body('user', User, UserSchema, required=True)
+@io.render(schema=UserSchema)
+def add_user(user):
+    return user
+
+
+@app.errorhandler(ValidationError)
+def validation_handler(error):
+    response = jsonify(error_message=error.message)
+    response.status_code = 400
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
