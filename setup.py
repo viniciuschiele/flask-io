4c9fa7aa6a80afcb036a6c0f34453fa3ee9db01c
viniciuschiele <vinicius.chiele@gmail.com>
2015-09-30 11:09:48 +0100

    Release 1.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='flask-io',
-    version='1.3.1',
+    version='1.3.2',
     packages=['flask_io'],
     url='https://github.com/viniciuschiele/flask-io',
     license='Apache 2.0',
