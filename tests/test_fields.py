8abd3a7207dff9799a9012593e5edad6086d5280
Vinicius Chiele <vinicius.chiele@gmail.com>
2015-10-16 20:35:49 +0100

    Added unit tests for Enum field

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -12,14 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask_io.fields import Password
+from enum import Enum
+from flask_io import fields
 from flask_io.validate import ValidationError
 from unittest import TestCase
 
 
+class MyEnum(Enum):
+    member1 = 1
+    member2 = 2
+    member3 = 3
+
+
+class TestEnum(TestCase):
+    def test_serialize(self):
+        field = fields.Enum(MyEnum)
+
+        self.assertEqual(field.serialize('a', {'a': MyEnum.member2}), 2)
+
+    def test_deserialize(self):
+        field = fields.Enum(MyEnum)
+
+        self.assertEqual(field.deserialize(2), MyEnum.member2)
+
+
 class TestPassword(TestCase):
     def test_default_settings(self):
-        field = Password()
+        field = fields.Password()
 
         self.assertEqual('Pa4sw@rd', field.deserialize('Pa4sw@rd'))
         self.assertRaises(ValidationError, field.deserialize, 'pa4sw@rd')
