3ec76263ccc3a676c121ac12c7c4e71b391d889d
viniciuschiele <vinicius.chiele@gmail.com>
2015-12-10 14:04:16 +0000

    Add permission IsAuthenticated

--- flask_io/permissions.py
+++ flask_io/permissions.py
@@ -3,6 +3,7 @@ Provides a set of pluggable permission policies.
 """
 
 from abc import ABCMeta, abstractmethod
+from flask import request
 
 
 class Permission(metaclass=ABCMeta):
@@ -16,3 +17,14 @@ class Permission(metaclass=ABCMeta):
         Return `True` if permission is granted, `False` otherwise.
         """
         pass
+
+
+class IsAuthenticated(Permission):
+    """
+    Allows access only to authenticated users.
+    """
+
+    def has_permission(self):
+        if not hasattr(request, 'user'):
+            return False
+        return request.user is not None
